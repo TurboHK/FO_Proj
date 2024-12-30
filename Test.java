0640fe9fce3123b039b4266ace7df4ba3d780d52
TurboHK <59258447+TurboHK@users.noreply.github.com>
2024-12-30 23:39:09 +0800

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+	public static void main(String[] args) {
+		Person.testPerson();
+		Student.testStudent();
+		Employee.testEmployee();
+		FacultyMember.testFacultyMember();
+		FinanceOffice.testFinanceOffice();
+	}
+}
